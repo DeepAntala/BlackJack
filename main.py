73b2df231917276183b0eaefa366599acf318e9a
DeepAntala <deep.antala@yahoo.com>
2020-09-21 23:08:23 +0400

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ ranks = ['Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight',
 
 values = {'Two':2, 'Three':3, 'Four':4, 'Five':5, 'Six':6, 'Seven':7, 'Eight':8,
           'Nine':9, 'Ten':10, 'Jack':11, 'Queen':12, 'King':13, 'Ace':14}
+playing = True
 
 class Card:
     def __init__(self,suit,rank):
@@ -91,6 +92,113 @@ def hit(deck,hand):
     single_card=deck.deal()
     hand.add_card(single_card)
     hand.adjust_for_ace()
+
+def hit_or_stand(deck,hand):
+    global playing
+    
+    while True:
+        x = input("Hit or Stand? Enter h or s ")
+        if x[0].lower() == 'h':
+            hit(deck,hand)
+        elif x[0].lower() == 's':
+            print("Player Stands Dealer's Turn")
+            playing = False
+            
+        else:
+            print("Sorry, I did not understand that, Please enter h or s only!")
+        
+        break;
+    
+def show_some(player, dealer):
+    print("DEALERS HAND:")
+    print("one card hidden!")
+    print(dealer.cards[1])
+    print("\n")
+    print("PLAYERS HAND:")
+    for card in player.cards:
+        print(card)
+        
+def show_all(player, dealer):
+    print("DEALERS HAND:")
+    for card in dealer.cards:
+        print(card)
+    print("\n")
+    print("PLAYERS HAND:")
+    for card in player.cards:
+        print(card)
+    
+def player_busts(player, deal, chips):
+    print("BUST PLAYER!")
+    chips.lose_bet()
+    
+def player_wins(player, deal, chips):
+    print("PLAYER WINS")
+    chips.win_bet()
+
+def dealer_busts(player, deal, chips):
+    print("PLAYER WINS! DEALER BUSTED!")
+    chips.win_bet()
+    
+def dealer_wins(player, deal, chips):
+    print("DEALER WINS!")
+    chips.lose_bet()
+
+def push(player, deal):
+    print("Dealer and player tie! PUSH")
+    
+while True:
+    print("WELCOME TO BLACKJACK")
+    deck = Deck()
+    deck.shuffle()
+    
+    player_hand = Hand()
+    player_hand.add_card(deck.deal())
+    player_hand.add_card(deck.deal())
+    
+
+    dealer_hand = Hand()
+    dealer_hand.add_card(deck.deal())
+    dealer_hand.add_card(deck.deal())   
+    
+    player_chips = Chips()
+    
+    take_bet(player_chips)
+    
+    show_some(player_hand,dealer_hand)
+    
+    while playing:
+        hit_or_stand(deck,player_hand)
+        show_some(player_hand, dealer_hand)
+        
+        if player_hand.value > 21:
+            player_busts(player_hand, dealer_hand, player_chips)
+            break
+    if player_hand.value <= 21:
+        while dealer_hand.value < player_hand.value:
+            hit(deck, dealer_hand)
+            
+        show_all(player_hand, dealer_hand)
+        
+        if dealer_hand.value > 21:
+            dealer_busts(player_hand, dealer_hand, player_chips)
+        elif dealer_hand.value > player_hand.value:
+            dealer_wins(player_hand, dealer_hand, player_chips)
+        elif dealer_hand.value < player_hand.value:
+            player_wins(player_hand, dealer_hand, player_chips)
+        else:
+            push(player_hand, dealer_hand)
+    print("\n Player total chips are at: {}".format(player_chips.total))
+    
+    new_game = input("Would you like to play another hand? y/n")
+    if new_game[0].lower() == "y":
+        player = True
+        continue
+    else:
+        print("Thank you for playing")
+        break
+
+            
+
 
             
 
